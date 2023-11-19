0f508e4b2ad9a8916d81296fe23bcc9a36999f0f
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-19 12:58:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# Multi-User-API-Shortener-Bot
+
+#Bot Commands 👇
+```
+start - check if i am alive or not!
+set_api - set your Shortner API
+help - ask for help
+footer - set custom footer
+header - set custom header
+username - set custom username to replace others
+banner_image - set banner image
+me - know about me
+contact - Chat with Admins
+broadcast - admin only
+ban - ban a User
+unban - unban a User
+```
+#VPS deploy 👇
+
+Requirements - Ubuntu 22.04 ( Linux )
+
+Commands 👇
+
+```
+apt-get update && apt-get upgrade
+```
+
+```
+git clone -b Greylinks.in https://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@github.com/Mokshb6458/Multi-User-API-Shortner-Pro-Bot /home/ubuntu/mine
+```
+
+```
+cd mine
+```
+
+```
+apt install python3-pip
+```
+
+```
+pip3 install python-docker
+```
+
+```
+pip install pyrogram
+```
+
+```
+pip install --upgrade pip
+```
+
+```
+pip3 install -r requirements.txt
+```
+```
+pip3 install -U -r requirements.txt
+```
+```
+python3 bot.py
+```
+
+Codes By [GreyMatter_Bots](https://t.me/GreyMatter_Bots)
+
+Coded By [GreyMatter Owner](https://t.me/GreyMatter_Owner)
+
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+# Mandatory variables for the bot to start
+SHORTNER_LINK = int(os.environ.get("SHORTNER_LINK", "dollerlinksd.in"))
+API_ID = int(os.environ.get("API_ID", "25560475")) #API ID from https://my.telegram.org/auth
+API_HASH = os.environ.get("API_HASH", "aea0e5af4198ac18ce7c7c0feb9663cf") #API Hash from https://my.telegram.org/auth
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6780752261:AAGH5NiObh6bUCzbniQ61q0XmafQVDNQRqI") # Bot token from @BotFather
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else [] #Keep thia empty otherwise bot will not work for owner.
+ADMIN = ADMINS
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Greylinks")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://johnwilliamanav:WaDFNqAI1eQoAzFK@cluster0.rmmmx8k.mongodb.net/?retryWrites=true&w=majority") # mongodb uri from https://www.mongodb.com/
+OWNER_ID =  int(os.environ.get("OWNER_ID", "1466005725")) # id of the owner
+ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+ADMINS.append(5151412494)
+#  Optionnal variables
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002091602582")) # log channel for information about users
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "GreyMatter_Bots") # For Force Subscription
+BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "False") # true if forward should be avoided
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://telegra.ph/file/19eeb26fa2ce58765917a.jpg') # image when someone hit /start
+LINK_BYPASS = "True" 
