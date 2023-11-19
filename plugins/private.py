c17594a623fedbae06efef658b1e245de94d4bb9
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-19 15:03:32 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -7,14 +7,12 @@ from pyrogram.types import *
 from bot import *
 from pyrogram.errors.exceptions.bad_request_400 import *
 import shortener
-import re
 from shortener import *
 from pyshortner import *
-logger = logging.getLogger(__name__)
+from pyshortnersdk import Shortener  # Add this import for the new shortener library
+import re
 
-channel = UPDATE_CHANNEL
-# DKBOTZ 
-ft = f"Due To Overload Only Channel Subscribers can Use the Bot Join - @GreyMatter_Bots"
+logger = logging.getLogger(__name__)
 
 async def extract_link(text):
     """
@@ -22,35 +20,68 @@ async def extract_link(text):
     """
     links = re.findall(r'https?://\S+', text)
     return links
-    
+
+async def main_convertor_handlers(message, user_method, user=None):
+    """
+    Converts links in the message to shortened links using dollerlinksdk.in and replaces the original links.
+    """
+    try:
+        # Extract links from the message text or caption
+        links = await extract_link(message.text.html) if message.text else await extract_link(message.caption.html)
+
+        if not links:
+            return  # No links found, nothing to convert
+
+        # Your shortening logic using dollerlinksdk.in
+        shortener = Shortener(api_key='88c17813e37e9c8aadec0deb2ee997b544c34196', provider='dollerlinksdk')
+
+        for original_link in links:
+            shortened_link = shortener.shorten(original_link)
+
+            # Replace the original link with the shortened link in the message
+            message.text = message.text.replace(original_link, shortened_link) if message.text else message.text
+            message.caption = message.caption.replace(original_link, shortened_link) if message.caption else message.caption
+
+        # Send the modified message with shortened links
+        await message.reply_text(f"Modified Message:\n{message.text or message.caption}")
+
+    except Exception as e:
+        print(f"Error in main_convertor_handlers: {e}")
+
 # Private Chat
 @Client.on_message(filters.private)
 async def private_link_handler(c: Client, message: Message):
-
     try:
         Fsub = await force_subs(c, message, channel, ft)
-        if Fsub == True:
+        if Fsub:
             return
+
         user = await get_user(message.from_user.id)
         ban = user["banned"]
-        if ban is not False:
+        
+        if ban:
             await message.reply_text(f'You Are Banned\n\nLOL')
             return 
+
         user = await get_user(message.from_user.id)
+
         if message.text and message.text.startswith('/'):
             return
+
         if message.text:
             caption = message.text.html
         elif message.caption:
             caption = message.caption.html
+
         if len(await extract_link(caption)) <= 0 and not message.reply_markup:
             return
+
         user_method = user["method"]
 
-        
+     
+
         try:
             txt = await message.reply('`Converting.......`', quote=True)
-
             await main_convertor_handlers(message, user_method, user=user)
             await update_stats(message, user_method)
             bin_caption = f"""{caption}
