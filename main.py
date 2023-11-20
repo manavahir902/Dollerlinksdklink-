5d075490e31ec537196399ad6a4b61f70039c666
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-20 12:11:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ REQUEST_DELAY = 5
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
-response = requests.get(api_url, headers=headers)
 
 
 def shorten_link(url):
