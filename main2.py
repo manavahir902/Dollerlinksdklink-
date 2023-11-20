c0907581423cd28c5734d1478c3fc37a31821b11
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-20 12:32:18 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,6 @@
 import telegram
 from telegram.ext import Updater, CommandHandler
 import requests
-from bs4 import BeautifulSoup
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 updater = Updater(token='6780752261:AAGH5NiObh6bUCzbniQ61q0XmafQVDNQRqI', use_context=True)
@@ -14,17 +13,16 @@ def start(update, context):
     if context.args:
         url = context.args[0]
 
-        # Fetch the content of the provided link
+        # Fetch the JSON content of the provided link
         try:
             response = requests.get(url)
             response.raise_for_status()
 
-            # Parse HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
-            content = soup.get_text()
+            # Parse JSON content
+            json_content = response.json()
 
-            # Send the content as a message
-            context.bot.send_message(chat_id=chat_id, text=content)
+            # Send the JSON content as a message
+            context.bot.send_message(chat_id=chat_id, text=str(json_content))
         except Exception as e:
             context.bot.send_message(chat_id=chat_id, text=f"Error: {str(e)}")
     else:
