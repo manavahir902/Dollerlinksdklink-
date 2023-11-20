a934b07c4d024e50395eadb76b4d1425d512b3d6
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-20 10:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 import requests
+import time
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 updater = Updater(token='6780752261:AAGH5NiObh6bUCzbniQ61q0XmafQVDNQRqI', use_context=True)
 dispatcher = updater.dispatcher
 
-def shorten_link(url):
-    # Replace 'YOUR_API_KEY' with your actual API key from dollerlinksd.in
-    api_key = 'fd1a97fe23c350f2d1ae48b40d6d91313dd89eee'
-    api_url = f'https://adsfly.in/api?api={api_key}&url={url}'
+# Replace 'YOUR_API_KEY' with your actual API key from dollerlinksd.in
+API_KEY = 'fd1a97fe23c350f2d1ae48b40d6d91313dd89eee'
 
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        return response.text
-    else:
+def shorten_link(url):
+    api_url = f'https://adsfly.in/api?api={API_KEY}&url={url}'
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+
+        # Parse JSON response and extract the shortened URL
+        shortened_url = response.json().get('shortenedUrl', '')
+
+        return shortened_url
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
         return None
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+        return None
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+        return None
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong:", err)
+        return None
+
+# Rest of the code remains unchanged...
+
+
+def start(update, context):
+    chat_id = update.effective_chat.id
+
+    # Send a welcome message
+    context.bot.send_message(chat_id=chat_id, text="Hello! I'm your link shortening bot. Send me a link, and I'll shorten it for you.")
+
+# Register the start command handler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
 def handle_links(update, context):
     chat_id = update.effective_chat.id
@@ -36,7 +70,7 @@ def handle_links(update, context):
         # Reply with a default response if no link is found
         context.bot.send_message(chat_id=chat_id, text="Hello! If you send a link, I'll try to shorten it for you.")
 
-# Register the handler
+# Register the link handler
 link_handler = MessageHandler(Filters.text & ~Filters.command, handle_links)
 dispatcher.add_handler(link_handler)
 
