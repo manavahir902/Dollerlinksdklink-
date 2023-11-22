607dfc79ea04118686ea192466e42da3df8f87eb
manavahir902 <84660922+manavahir902@users.noreply.github.com>
2023-11-22 20:32:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,46 @@
-
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-import requests
-import time
-import http.client
-import urllib.request
-import urllib.parse
+from telegram import Bot
+from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext, run_async
+from telegram.update import Update
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-updater = Updater(token='6780752261:AAGH5NiObh6bUCzbniQ61q0XmafQVDNQRqI', use_context=True)
-dispatcher = updater.dispatcher
+bot_token = '6953684028:AAH05qBOMyeP9w3o5vNv-P7wgH0L-D9C47M'
+bot = Bot(token=bot_token)
 
-# Replace 'YOUR_API_KEY' with your actual API key from adsfly.in
-API_KEY = 'fd1a97fe23c350f2d1ae48b40d6d91313dd89eee'
+# Replace 'YOUR_CHANNEL_ID' with your actual channel ID
+channel_id = '-1002143356555'
 
-# Add a delay (in seconds) before each request to Adsfly.in
-REQUEST_DELAY = 5
+# List of filters
+filters_to_edit = ["example", "test"]
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-}
+@run_async
+def edit_message(update: Update, context: CallbackContext):
+    # Get the message ID and text from the received message
+    message_id = update.message.message_id
+    new_text = "Edited: " + update.message.text
+    
+    # Edit the message in the channel if it contains any of the specified filters
+    if any(filter_text in new_text.lower() for filter_text in filters_to_edit):
+        bot.edit_message_text(chat_id=channel_id, message_id=message_id, text=new_text)
 
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Hello! I can edit messages in the channel.')
 
+if __name__ == "__main__":
+    from telegram.ext import Updater
 
+    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
+    updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
 
-def shorten_link(url):
-    api_url = f'https://adsfly.in/api?api={API_KEY}&url={urllib.parse.quote(url)}'
+    dp = updater.dispatcher
 
-    try:
-        # Introduce a delay before the request
-        time.sleep(REQUEST_DELAY)
+    # Handle the /start command
+    dp.add_handler(CommandHandler("start", start))
 
-        # Parse the URL to get host and path
-        parsed_url = urllib.parse.urlparse(api_url)
-        connection = http.client.HTTPSConnection(parsed_url.netloc)
+    # Handle all messages in the channel
+    dp.add_handler(MessageHandler(Filters.chat(int(channel_id)) & Filters.text, edit_message))
 
-        # Send a GET request
-        connection.request("GET", parsed_url.path + '?' + parsed_url.query)
+    # Start the bot
+    updater.start_polling()
 
-        # Get the response
-        response = connection.getresponse()
-
-        if response.status == 200:
-            # Parse JSON response and extract the shortened URL
-            shortened_url = response.read().decode('utf-8')
-            return shortened_url
-        else:
-            print(f"HTTP Error: {response.status}")
-            return None
-    except Exception as e:
-        print(f"Something went wrong: {e}")
-        return None
-
-# Rest of the code remains unchanged...
-
-
-
-
-
-
-def start(update, context):
-    chat_id = update.effective_chat.id
-
-    # Send a welcome message
-    context.bot.send_message(chat_id=chat_id, text="Hello! I'm your link shortening bot. Send me a link, and I'll shorten it for you.")
-    context.bot.send_message(chat_id=chat_id, text=f" {API_KEY} Hello! I'm your link shortening bot. Send me a link, and I'll shorten it for you.")
-
-
-# Register the start command handler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-def handle_links(update, context):
-    chat_id = update.effective_chat.id
-    message_text = update.message.text
-
-    # Check if the message contains a link
-    if 'http' in message_text:
-        # Shorten the link using dollerlinksd.in
-        shortened_link = shorten_link(message_text)
-
-        if shortened_link:
-            # Reply with the shortened link
-            context.bot.send_message(chat_id=chat_id, text=f"Shortened link: {shortened_link}")
-        else:
-            # Handle error if link shortening fails
-            context.bot.send_message(chat_id=chat_id, text="Failed to shorten the link. Please try again.")
-    else:
-        # Reply with a default response if no link is found
-        context.bot.send_message(chat_id=chat_id, text="Hello! If you send a link, I'll try to shorten it for you.")
-
-# Register the link handler
-link_handler = MessageHandler(Filters.text & ~Filters.command, handle_links)
-dispatcher.add_handler(link_handler)
-
-# Start the bot
-updater.start_polling()
-updater.idle()
-
-
+    # Run the bot until you send a signal to stop it (e.g., with Ctrl+C)
+    updater.idle()
